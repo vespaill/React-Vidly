f79001ab22498611822b5b4074de6ab1363d4760
Victor Espaillat <vicander_99@hotmail.com>
2020-06-12 17:58:37 -0400

    raising & handling evnts, updating state

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
   state = {
-    value: this.props.value
+    value: this.props.counter.value
   };
 
   handleIncrement = product => {
@@ -11,16 +11,20 @@ class Counter extends Component {
   };
 
   render() {
-    console.log(this.props);
     return (
       <div>
-        {this.props.children}
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
           onClick={() => this.handleIncrement({ id: 1 })}
           className="btn btn-secondary btn-sm"
         >
           Increment
+        </button>
+        <button
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
         </button>
       </div>
     );
