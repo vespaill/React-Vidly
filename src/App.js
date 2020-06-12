a470ae8024e871999c61712c5d875da40e753e15
Victor Espaillat <vicander_99@hotmail.com>
2020-06-12 18:27:26 -0400

    Multiple components in sync, lifting state up

--- src/App.js
+++ src/App.js
@@ -1,15 +1,59 @@
-import React from 'react';
+import React, { Component } from 'react';
+import NavBar from './components/navbar';
 import './App.css';
-// import Movies from './components/movies';
 import Counters from './components/counters';
 
-function App() {
-  return (
-    <main className="container">
-      {/* <Movies /> */}
-      <Counters />
-    </main>
-  );
+class App extends Component {
+  state = {
+    counters: [
+      { id: 1, value: 4 },
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 }
+    ]
+  };
+
+  handleIncrement = counter => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+
+    counters[index] = { ...counter };
+    counters[index].value++;
+
+    this.setState({ counters });
+  };
+
+  handleDelete = counterId => {
+    const counters = this.state.counters.filter(c => c.id !== counterId);
+    this.setState({ counters });
+  };
+
+  handleReset = () => {
+    const counters = this.state.counters.map(c => {
+      c.value = 0;
+      return c;
+    });
+
+    this.setState({ counters });
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <NavBar
+          totalCounters={this.state.counters.filter(c => c.value > 0).length}
+        />
+        <main className="container">
+          <Counters
+            counters={this.state.counters}
+            onReset={this.handleReset}
+            onIncrement={this.handleIncrement}
+            onDelete={this.handleDelete}
+          />
+        </main>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
